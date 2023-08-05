46c5f2667976134a237afbef17e72397af9269e7
BrunoDark777 <106707123+BrunoDark777@users.noreply.github.com>
2023-08-05 04:22:14 -0300

    Repetições em JavaScript

---
+++ aula13/ambiente.js
@@ -0,0 +1,10 @@
+/*let c = 1
+while (c < 6){
+console.log(`passo ${c}`)
+c++
+} */
+
+let c = 1 
+do{
+    console.log(`passo ${c}`)
+} while(c <= 6)
---
+++ aula14/ambiente.js
@@ -0,0 +1,16 @@
+/*let c = 1
+while(c<=5){
+    console.log(`passo ${c}`)
+    c++
+}*/
+
+/*let c = 1 
+do{
+    console.log(`passo ${c}`)
+    c++
+}while(c<=5)*/
+
+for(let c = 1; c<=5; c++){
+    console.log(`passo ${c}`)
+}
+
